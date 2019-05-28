8a166300e9fca0c6fd88094fe7385c0ea821db52
Rob <robjharris37@gmail.com>
2019-05-28 22:07:14 +0200

    Create modes.py
    created first module

---
+++ modes.py
@@ -0,0 +1,53 @@
+'''
+Modes module
+
+
+
+'''
+
+import numpy as np
+
+
+def calc_modes_circ(chi = 3., Lmbda = 0.5e-6,d_t = 0.7):
+    '''
+    The Mode equation for circular aperture
+
+    chi should be in arcsec
+    d_t in m
+    Lmbda in m
+
+    NOT ACCOUNTING FOR POLARISATION
+    '''
+
+    chi_rads = (chi/3600.)*numpy.pi/180.
+    M = (numpy.pi* chi_rads*d_t/(4.*Lmbda))**2.
+
+    return M
+
+def calc_modes_circ_NA(NA = 0.14, Lmbda = 0.5e-6, a = 5e-6):
+    '''
+    mode equation using fibre, not including polarisation
+
+    NA : Numerical aperture
+    Lmbda : Wavelength
+    a = radias of fibre core
+    '''
+
+    V = calc_V(NA,Lmbda,a)
+
+    M = V**2/4
+
+    return M
+
+def calc_V(NA = 0.14, Lmbda = 0.5e-6, a = 5e-6):
+    '''
+    calculate the V parameter for a fibre
+    
+    NA = numerical aperture
+    Lmbda = Wavelength
+    a = core radius
+    '''
+
+    V = 2*numpy.pi*a*NA/Lmbda
+
+    return V
